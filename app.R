ef27f7f5d6aa107bc487020ecd7947f680a8a1ce
Anders <landers@illumina.com>
2022-03-13 10:26:27 -0700

    Chatbot selection image and flavor text updating

--- app.R
+++ app.R
@@ -8,9 +8,46 @@ library(tidyr)
 library(shinydashboard)
 library(shinythemes)
 library(odbc)
+library(shinyFeedback)
 
 # Load chatbot function(s)
 source("chatbot_leafey.R")
+
+# #Connect server code snippet
+# #Check operating system, if linux log in as root user for being able to initialize authentication request to databases.
+# #Use the Connect UI to create system variables with username and password for appropriate account
+# #Credit to: https://conjugateprior.org/2015/06/identifying-the-os-from-r/
+# get_os <- function(){
+#   sysinf <- Sys.info()
+#   if (!is.null(sysinf)){
+#     os <- sysinf['sysname']
+#     if (os == 'Darwin')
+#       os <- "osx"
+#   } else { ## mystery machine
+#     os <- .Platform$OS.type
+#     if (grepl("^darwin", R.version$os))
+#       os <- "osx"
+#     if (grepl("linux-gnu", R.version$os))
+#       os <- "linux"
+#   }
+#   tolower(os)
+# }
+# 
+# #For development: set locally your system variables for username and password
+# #Sys.setenv(username = "my_username")
+# #Sys.setenv(password = "my_password")
+# 
+# # #Check if system is on linux, if so put in the supplied username and password to sudo the account for successful database authentication
+# if(get_os() == "linux") {
+#   message("Linux OS, putting in developer credentials")
+#   
+#   #Get developer info using environmental variables entered using the UI
+#   username <- Sys.getenv("username")
+#   password <- Sys.getenv("password")
+#   inputs = c(password)
+#   command = paste0('su - ', username)
+#   system(command, input = inputs)
+# }
 
 # Define UI  
 ui <- shinyUI(fluidPage(theme = shinytheme("spacelab"), 
@@ -42,14 +79,33 @@ ui <- shinyUI(fluidPage(theme = shinytheme("spacelab"),
       
     ),
     
-    # Main panel for displaying outputs - Custom handling for loading gif when chatbot is "thinking"
+    # Main panel for displaying outputs - Custom handling for loading gif when chatbot is "thinking" after submit button is clicked. This will need to be updated. 
     mainPanel(
       
       actionButton(inputId = "Submit",
                    label = "Start chatting"
-      ),
-      
-      tags$head(tags$style(type="text/css", "
+      )
+    )
+  ),
+  
+  tags$head(tags$style(
+    type="text/css",
+    "#chatbotImg img {max-width: 100%; width: 100%; height: auto}"
+  )),
+  
+  sidebarPanel(
+    #Display chosen chatbot name and image 
+    textOutput("text"),
+    
+    imageOutput("chatbotImg"),
+    
+    width = 2),
+  
+  mainPanel(
+
+    DTOutput("table"),
+    
+    tags$head(tags$style(type="text/css", "
                              #loadmessage {
                                position: fixed;
                                top: 0px;
@@ -64,28 +120,11 @@ ui <- shinyUI(fluidPage(theme = shinytheme("spacelab"),
                                z-index: 105;
                              }
                           ")),
-      
-      conditionalPanel(
-        condition="($('html').hasClass('shiny-busy'))",
-        tags$img(src=busy)
-      )
+    
+    conditionalPanel(
+      condition="($('html').hasClass('shiny-busy'))",
+      tags$img(src=busy)
     )
-  ),
-  
-  tags$head(tags$style(
-    type="text/css",
-    "#chatbotImg img {max-width: 100%; width: 100%; height: auto}"
-  )),
-  
-  sidebarPanel(
-    
-    imageOutput("chatbotImg"),
-    
-    width = 2),
-  
-  mainPanel(
-
-    DTOutput("table")
     
   )
 )
@@ -94,48 +133,25 @@ ui <- shinyUI(fluidPage(theme = shinytheme("spacelab"),
 # Define server logic  
 server <- function(input, output) {
   
-  #Connect server code snippet 
-  #Check operating system, if linux log in as root user for being able to initialize authentication request to databases. 
-  #Use the Connect UI to create system variables with username and password for appropriate account
-  #Credit to: https://conjugateprior.org/2015/06/identifying-the-os-from-r/ 
-  get_os <- function(){
-    sysinf <- Sys.info()
-    if (!is.null(sysinf)){
-      os <- sysinf['sysname']
-      if (os == 'Darwin')
-        os <- "osx"
-    } else { ## mystery machine
-      os <- .Platform$OS.type
-      if (grepl("^darwin", R.version$os))
-        os <- "osx"
-      if (grepl("linux-gnu", R.version$os))
-        os <- "linux"
-    }
-    tolower(os)
-  }
+  # #Error checking and provide feedback to user
+  # chatbot_ready_warning <- reactive({
+  #   valid <- input$chatbot %in% c("notleafey") #c("Leafey")
+  #   shinyFeedback::feedbackWarning("n", !valid, "Please select a chatbot ready to talk with")
+  #   
+  #   # even <- input$n %% 2 == 0
+  #   # shinyFeedback::feedbackWarning("n", !even, "Please select an even number")
+  #   # input$n / 2    
+  # })
   
-  #For development: set locally your system variables for username and password
-  #Sys.setenv(username = "my_username")
-  #Sys.setenv(password = "my_password")
+  #output$half <- renderText(half())
   
-  # #Check if system is on linux, if so put in the supplied username and password to sudo the account for successful database authentication
-  if(get_os() == "linux") {
-    message("Linux OS, putting in developer credentials")
-    
-    #Get developer info using environmental variables entered using the UI
-    username <- Sys.getenv("username")
-    password <- Sys.getenv("password")
-    inputs = c(password)
-    command = paste0('su - ', username)
-    system(command, input = inputs)
-  }
-  
-  #Display chatbot image 
+  #Display initial chatbot image 
   output$chatbotImg <- renderImage({
-    filename <- normalizePath(file.path('./files', paste(input$chatbot,'.PNG', sep='')))
+    filename <- normalizePath(file.path('./files', paste('default','.PNG', sep='')))
     list(src = filename, alt = paste("Chatbot Profile Image"))
   }, deleteFile = FALSE)
-  
+
+  output$text <- renderText({ "Select a Chatbot from the list above" })
   
   #Start out with blank text as a placeholder
   #output$timed <- renderText({ NULL })
@@ -148,7 +164,14 @@ server <- function(input, output) {
   
   observeEvent(input$Submit,{
     
-    timed_t0 <- Sys.time()
+    #Display updated chatbot image based on who user selected
+    output$chatbotImg <- renderImage({
+      filename <- normalizePath(file.path('./files', paste(input$chatbot,'.PNG', sep='')))
+      list(src = filename, alt = paste("Chatbot Profile Image"))
+    }, deleteFile = FALSE)
+    
+    #Update flavor text for chosen Chatbot
+    output$text <- renderText({ paste("You have chosen: ", input$chatbot ) })
     
     if(input$chatbot == "Leafey") {
       df <- data.frame("Chatbot" = c("Leafey"), "Directory" = c(getwd()))
